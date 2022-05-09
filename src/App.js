8da0bf17114023777a9ccd089b9460f7287d42a7
ronniejuan <hofmannronnie@gmail.com>
2022-05-09 13:39:37 +0200

    dark-light mode working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+import {Navbar} from './components/Navbar'
+import {Routes} from './components/Routes'
+import {Footer} from './components/Footer'
+
 
 function App() {
+    const [darkTheme,setDarkTheme] = useState(false)
+
     return (
-        <div>
-            <p class="text-green-900">The quick brown fox...</p>
-            hola que tal
+        <div className={darkTheme ? 'dark' :''} >
+            <div className="bg-gray-00 dark:bg-gray-900 dark:text-gray-200 min-h-screen" >
+                 <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+                 <Routes />
+                 <Footer />
+            </div>
+
         </div>
     )
 }
