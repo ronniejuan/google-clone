ab56166013a8f89f9035baedd39556a0a638152c
ronniejuan <hofmannronnie@gmail.com>
2022-05-10 16:32:55 +0200

    56:41

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -24,7 +24,24 @@ export const Results = () => {
 switch (location.pathname) {
 
         case '/search':
-            return  'SEARCH'
+            return  (
+                <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
+                    {results?.results?.map(({link,title, description},index)=>(
+                        <div key={index} className="md:w-2/5 w-full">
+                            <a href={link} target="_blank" rel="noreferrer">
+                                <p className="text-sm">
+                                    {link.lenght > 30 ? link.substring(0, 30) : link}
+                                </p>
+                                <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
+                                    {title}
+                                </p>
+                            </a>
+                        </div>
+                         
+                    ))}
+
+                </div>
+            )
         case '/images':
             return  'SEARCH'
         case '/news':
