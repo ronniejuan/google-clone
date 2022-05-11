4dd400d3ec99fb98dd4a18e0dccef00fd38758ae
ronniejuan <hofmannronnie@gmail.com>
2022-05-11 16:48:45 +0200

    pushing

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -13,8 +13,13 @@ export const Results = () => {
     
     
     useEffect(() => {
-        getResults(`/search/q=JavaScript Mastery&num=40`);
-    },[])
+        if(searchTerm){
+            if(location.pathname=== '/videos'){
+                getResults(`/search/q=${searchTerm} videos`);
+            }
+        }
+        getResults(`${location.pathname}/q=${searchTerm}&num=40`);
+    },[searchTerm,location.pathname,getResults]);
 
 
 
@@ -43,7 +48,22 @@ switch (location.pathname) {
                 </div>
             )
         case '/images':
-            return  'SEARCH'
+            return  (
+                <div className="flex flex-wrap justify-content items-center">
+                    {results?.image_results?.map(({image,link:{href,title },index})=>(
+                        <a className="sm:p-3 p-5" href={href} key={index} target="_blank" rel="noreferrer" >
+                            <img src={image?.src}  alt={title} loading="lazy" />
+                            <p className="w-36 break-words text-sm mt-2"> 
+                            {title}
+                            
+                            </p>
+                        </a>
+
+                    ))}
+
+                </div>
+                
+            )
         case '/news':
             return  'SEARCH'
         case '/videos':
